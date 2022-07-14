ce99957775b2c795d3e95ffd02f302d9ba99d862
Obalola Hammed Abimbola <108901770+ObalolaAH@users.noreply.github.com>
2022-07-14 01:44:18 +0000

    JavaScript Function Assignment

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+/*
+ function sumOfNumbers(arrayOfNumbers) {
+    let sum = 0;
+ 
+    for (let i=0; i<arrayOfNumbers.length; i++) {
+         sum += arrayOfNumbers[i]; 
+         }
+     return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
+*/
 
 /**
  * Challenge - 2
@@ -28,9 +36,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+    for (let i=0; i< arrayOfNumbers.length; i++){
+      if (arrayOfNumbers[i] % 2 === 0) {
+        count++
+      }
+    }
+  return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +61,13 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+   return arrayOfNumbers.map(
+     function (celsius) {
+       return Math.trunc(celsius * 1.8 + 32)
+     }
+    );
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
